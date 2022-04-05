b1bda5a5a95c9a0486cb1661bc499501ce94d764
Ratnavo <ratnavo.dutta@hotmail.com>
2022-04-05 22:25:05 +0530

    commit

---
+++ methods-function-practice.py
@@ -0,0 +1,33 @@
+from ensurepip import version
+
+
+# Learning From Pierian Data
+# Learning Methods
+mylist = [1,2,3]
+mylist.append(4)
+print(mylist)
+
+# Methods are built-in objects in Python
+mylist.pop()
+print(mylist)
+
+#==================================================
+
+# Learning how to use function
+# def - Keyword telling Python this is a function
+# def name_of_function():
+#         "Docstring explain function"
+# above statement "name_of_function" - known as snake casing where we use all lowercase with underscores between words.
+# () stands for parenthesis which is used here at the end. Later on we can pass in arguments/parameters into the fucntion.
+# Function can accept arguments to be passed by the user.
+# return = this is a keyword that allows to use the output of a function to a new vairable.
+
+
+
+#==================================================
+
+
+
+
+
+
